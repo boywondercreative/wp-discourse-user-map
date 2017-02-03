488f07e2acb15da852ebc6d2f24bef7ef86844ae
scossar <scossar3@gmail.com>
2017-02-02 20:31:42 -0800

    Use user info instead of fields

--- discourse-user-map.php
+++ discourse-user-map.php
@@ -21,12 +21,6 @@ class DiscourseUserMap {
 		add_action( 'rest_api_init', array( $this, 'initialize_location_route' ) );
 	}
 
-	public function add_template_to_dropdown( $posts_templates ) {
-		$posts_templates = array_merge( $posts_templates, $this->templates );
-
-		return $posts_templates;
-	}
-
 	public function initialize_location_route() {
 		register_rest_route( 'scossar/v1', '/user-location', array(
 			'methods'  => 'POST',
@@ -42,33 +36,29 @@ class DiscourseUserMap {
 
 			return null;
 		}
-		write_log( $data->get_json_params() );
-		$user_field_data  = $data->get_json_params()['user']['user_fields'];
-		$user_fields_info = $this->get_discourse_user_fields_info( 'http://localhost:3000' );
+		$request_array = $data->get_json_params();
 
-		$user_location = [];
+		if ( ! empty( $request_array['user'] ) && ! empty( $request_array['user']['location'] ) ) {
+			$user_data = $data->get_json_params()['user'];
+			$location = $user_data['location'];
+			$username = $user_data['username'];
 
-		foreach ( $user_fields_info as $field_info ) {
-			$this->process_user_field( 'city', $field_info, $user_field_data, $user_location );
-			$this->process_user_field( 'state/province', $field_info, $user_field_data, $user_location );
-			$this->process_user_field( 'country', $field_info, $user_field_data, $user_location );
-			$this->process_user_field( 'map', $field_info, $user_field_data, $user_location );
+			$marker_data = array(
+				'markername' => $username,
+				'geocode' => $location,
+			);
+
+			return \MMPAPI::add_marker( $marker_data );
 		}
 
-		if ( ! empty( $user_location['city'] ) || ! empty( $user_location['state/province'] ) || ! empty( $user_location['country'] ) ) {
-			$location = $user_location['city'] . ' ' . $user_location['state/province'] . ' ' . $user_location['country'];
-			write_log($location);
-		}
-
+		return null;
 	}
 
-	protected function process_user_field( $field_name, $field_info, $field_data, &$user_location ) {
-		if ( $field_name === strtolower( $field_info['name'] ) ) {
-			$id                           = $field_info['id'];
-			$user_location[ $field_name ] = isset( $field_data[ $id ] ) ? $field_data[ $id ] : '';
-		}
-	}
-
+	/**
+	 * @param $data
+	 *
+	 * @return \WP_Error|\WP_REST_Request
+	 */
 	protected function verify_discourse_request( $data ) {
 		if ( $sig = substr( $data->get_header( 'X-Discourse-Event-Signature' ), 7 ) ) {
 			$raw    = $data->get_body();
@@ -84,55 +74,6 @@ class DiscourseUserMap {
 			return new \WP_Error( 'Access Denied', 'Discourse Webhook Request Error: request did not originate from your Discourse webhook.' );
 		}
 	}
-
-	protected function get_discourse_user_fields_info( $discourse_url, $force_update = null ) {
-		$user_fields_info = get_transient( 'discourse_user_fields' );
-
-		if ( empty( $user_fields_info || $force_update ) ) {
-			$discourse_url = $discourse_url . '/site.json';
-			$remote        = wp_remote_get( $discourse_url );
-
-			if ( ! $this->validate_remote_get( $remote ) ) {
-
-				return 0;
-			}
-
-			$remote = json_decode( wp_remote_retrieve_body( $remote ), true );
-
-			if ( ! empty( $remote['user_fields'] ) ) {
-				$user_fields_info = $remote['user_fields'];
-
-				set_transient( 'discourse_user_fields', $user_fields_info, DAY_IN_SECONDS );
-			} else {
-
-				return new \WP_Error( 'key_not_found', 'The user_field key was not found in the response.' );
-			}
-		}
-
-		return $user_fields_info;
-	}
-
-	protected function validate_remote_get( $response ) {
-		if ( empty( $response ) ) {
-			error_log( 'Discourse has returned an empty response.' );
-
-			return 0;
-		} elseif ( is_wp_error( $response ) ) {
-			error_log( $response->get_error_message() );
-
-			return 0;
-
-			// There is a response from the server, but it's not what we're looking for.
-		} elseif ( intval( wp_remote_retrieve_response_code( $response ) ) !== 200 ) {
-			$error_message = wp_remote_retrieve_response_message( $response );
-			error_log( 'There has been a problem accessing your Discourse forum. Error Message: ' . $error_message );
-
-			return 0;
-		} else {
-			// Valid response.
-			return 1;
-		}
-	}
 }
 
-$discourse_user_map = new \DiscourseUserMap\DiscourseUserMap();
+new \DiscourseUserMap\DiscourseUserMap();
